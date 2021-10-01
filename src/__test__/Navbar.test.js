12799fb4464c275b0f7d3f33513ddee334356759
lily-coder <lilyondie@gmail.com>
2021-10-01 16:27:11 +0300

    Use react testing library to test user interaction

--- src/__test__/Navbar.test.js
+++ src/__test__/Navbar.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import renderer from 'react-test-renderer';
 import { BrowserRouter as Router } from 'react-router-dom';
+import '@testing-library/jest-dom/extend-expect.js';
 import Navbar from '../components/Navbar.js';
 
 it('renders Navbar Component correctly', () => {
--- src/__test__/Numbers.test.js
+++ src/__test__/Numbers.test.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-unused-vars */
 import renderer from 'react-test-renderer';
 import Numbers from '../components/Numbers.js';
+import '@testing-library/jest-dom/extend-expect.js';
 
 it('renders Numbers Component correctly', () => {
   const tree = renderer.create(<Numbers />).toJSON();
